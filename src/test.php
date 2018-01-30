e17360884175769edea6fa39911ee21147752080
Daniel Silva <danny@dannyps.net>
2018-01-30 10:39:36 +0000

    Rearrange folders

---
+++ src/test.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once("steamUser.class.php");
+
+
+use \Dannyps\Steam;
+
+var_dump( (new Steam\SteamUser("dannyps"))->getPrimaryGroupName());
+#echo (new SteamID("103582791429521412"))->profileInfo->getSteamName();
+ 
+echo PHP_EOL;
