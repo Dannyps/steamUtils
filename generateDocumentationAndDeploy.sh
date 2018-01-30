c0f55e6528758876c8da8837bcfc2b9c75172f20
Daniel Silva <danny@dannyps.net>
2018-01-30 00:18:22 +0000

    forgot to disable the html folder check

--- generateDocumentationAndDeploy.sh
+++ generateDocumentationAndDeploy.sh
@@ -76,7 +76,7 @@ cd $GH_REPO_NAME
 # Only upload if Doxygen successfully created the documentation.
 # Check this by verifying that the html directory and the file html/index.html
 # both exist. This is a good indication that Doxygen did it's work.
-if [ -d "html" ] && [ -f "html/index.html" ]; then
+if [ -f "index.html" ]; then
 
     echo 'Uploading documentation to the gh-pages branch...'
     # Add everything in this directory (the Doxygen code documentation) to the
