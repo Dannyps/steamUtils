250fedcba4e773bf397280f5de0789112865d495
Travis CI <travis@travis-ci.org>
2018-02-01 09:59:13 +0000

    Deploy code docs to GitHub Pages Travis build: 22
    Commit: ac0f437906623a8384d7af0c11d7c45b6af339ba

--- html/search/all_2.js
+++ html/search/all_2.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['are_20you_20dumb_3f_20_25steam_20has_20a_20new_20api_21',['Are you dumb? %Steam has a new API!',['../whyxml.html',1,'']]]
+  ['are_20you_20dumb_3f_20steam_20has_20a_20new_20api_21',['Are you dumb? Steam has a new API!',['../whyxml.html',1,'']]]
 ];
